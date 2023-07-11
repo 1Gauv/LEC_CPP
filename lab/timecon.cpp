25e18663cf26f63ddf40301a768ed7a68a11bef6
1Gauv <gauravjthapa@gmail.com>
2023-07-11 21:41:56 +0545

    assignment

---
+++ lab/timecon.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class time 
+{
+private:
+int hr;
+int min;
+int sec;
+
+public:
+time()
+{
+    hr=0;
+    min=0;
+    sec=0;
+
+}
+
+time()
+
+};
+
+int main()
+{
+
+return 0;
+}
